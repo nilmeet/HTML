f923619a6b5b595d79e98017b4e5bf6216203099
nilmeet <bhuvanilam.241192@gmail.com>
2022-09-21 21:57:46 -0700

    Add Schedule

---
+++ Loop.js
@@ -0,0 +1,10 @@
+var todo =[
+    "Morning worship",
+    "Exercise",
+    "Lecture Schedule",
+    "Meeting",
+    "Solving Issues"
+];
+for (var i=0; i < todo.length; i++){
+    console.log(todo[i]);
+}
